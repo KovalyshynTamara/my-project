e0bb7f4e7446c7c5214d87682e1ea091794c28c0
Tamara Kovalyshyn <kovtomara@gmail.com>
2023-03-13 18:56:03 +0200

    д

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -15,13 +15,13 @@ function Header() {
                 </Link>
                 <nav className="main-menu">
                 <ul>
-                    <li><Link to={'/'}>Home</Link></li>
-                    <li><Link to={'/solutions'}>Solutions</Link></li>
-                    <li><Link to={'/services'}>Services</Link></li>
-                    <li><Link to={'/about'}>About Us</Link></li>
-                    <li><Link to={'/blog'}>Blog</Link></li>
-                    <li><Link to={'/contacts'}>Contacts</Link></li>
-                    <li><Link to={'/team'}>Our Team</Link></li>
+                    <li><Link to={'/my-project/'}>Home</Link></li>
+                    <li><Link to={'/my-project/solutions'}>Solutions</Link></li>
+                    <li><Link to={'/my-project/services'}>Services</Link></li>
+                    <li><Link to={'/my-project/about'}>About Us</Link></li>
+                    <li><Link to={'/my-project/blog'}>Blog</Link></li>
+                    <li><Link to={'/my-project/contacts'}>Contacts</Link></li>
+                    <li><Link to={'/my-project//team'}>Our Team</Link></li>
                 </ul>
                 </nav>
 
@@ -32,18 +32,18 @@ function Header() {
         </div>
         : <div className="overlay">
             <div className="container small">
-                <Link to={'/'} className="logo">
+                <Link to={'/my-project/'} className="logo">
                 <img src={logo} alt="Logo"/>
                 </Link>
                 <nav className="main-menu">
                 <ul>
-                    <li><Link to={'/'}>Home</Link></li>
-                    <li><Link to={'/solutions'}>Solutions</Link></li>
-                    <li><Link to={'/services'}>Services</Link></li>
-                    <li><Link to={'/about'}>About Us</Link></li>
-                    <li><Link to={'/blog'}>Blog</Link></li>
-                    <li><Link to={'/contacts'}>Contacts</Link></li>
-                    <li><Link to={'/team'}>Our Team</Link></li>
+                    <li><Link to={'/my-project/'}>Home</Link></li>
+                    <li><Link to={'/my-project/solutions'}>Solutions</Link></li>
+                    <li><Link to={'/my-project/services'}>Services</Link></li>
+                    <li><Link to={'/my-project/about'}>About Us</Link></li>
+                    <li><Link to={'/my-project/blog'}>Blog</Link></li>
+                    <li><Link to={'/my-project/contacts'}>Contacts</Link></li>
+                    <li><Link to={'/my-project/team'}>Our Team</Link></li>
                 </ul>
                 </nav>
                 <div class="hmb-wrap" id=".hmb-wrap">
